a112a57279489e882b78bb2ad4dc5bedb38c2ee9
Ajay Chavan <ajayc9838@gmail.com>
2023-04-08 16:48:45 +0530

    implemented razorpay code

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+declare var Razorpay: any;
 
 @Component({
   selector: 'app-payment',
@@ -6,5 +8,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent {
+
+  message:any = "Not yet stared";
+  paymentId = "";
+  error = "";
+  title = 'Razorpay';
+  options = {
+    "key": "",
+    "amount": "200",
+    "name": "Abhijit Gatade",
+    "description": "Web Development",
+    "image": "https://www.abhijitgatade.com/assets/img/favicon.png",
+    "order_id": "fc2a36e30054ad786538566df9c7f003",
+    "handler": function (response: any) {
+      var event = new CustomEvent("payment.success",
+        {
+          detail: response,
+          bubbles: true,
+          cancelable: true
+        }
+      );
+      window.dispatchEvent(event);
+    },
+    "prefill": {
+      "name": "Ajay Chavan",
+      "email": "ajayc9838@gmail.com",
+      "contact": "12346789"
+    },
+    "notes": {
+      "address": "Navi Mumbai"
+    },
+    "theme": {
+      "color": "#3399cc"
+    }
+  };
+
+  paynow() {
+    this.paymentId = '';
+    this.error = '';
+    this.options.amount = "200"; //paise
+    this.options.prefill.name = "Abcd";
+    this.options.prefill.email = "abcd@gmail.com";
+    this.options.prefill.contact = "9999999999";
+    var rzp1 = new Razorpay(this.options);
+    rzp1.open();
+    rzp1.on('payment.failed', function (response: any) {
+      //this.message = "Payment Failed";
+      // Todo - store this information in the server
+      console.log(response.error.code);
+      console.log(response.error.description);
+      console.log(response.error.source);
+      console.log(response.error.step);
+      console.log(response.error.reason);
+      console.log(response.error.metadata.order_id);
+      console.log(response.error.metadata.payment_id);
+      //this.error = response.error.reason;
+    }
+    );
+  }
+  @HostListener('window:payment.success', ['$event'])
+  onPaymentSuccess(event: any): void {
+    this.message = "Success Payment";
+  }
 
 }
