02003b50280cbdae582425a0e2aa0369fb4dcd85
Ajay Chavan <ajayc9838@gmail.com>
2023-04-11 09:18:06 +0530

    setted path in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserLoginDashboardComponent } from './components/user-login-dashboard/user-login-dashboard.component';
+import { UserLoginHeaderComponent } from './components/user-login-header/user-login-header.component';
+import { RestaurantLoginComponent } from './components/restaurant-login/restaurant-login.component';
+import { RestaurantSignupComponent } from './components/restaurant-signup/restaurant-signup.component';
+import { RestaurantDashboardComponent } from './components/restaurant-dashboard/restaurant-dashboard.component';
+import { CartComponent } from './components/cart/cart.component';
+import { FavouriteComponent } from './components/favourite/favourite.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'userLogin',component:LoginComponent},
+  {path:'register',component:SignupComponent},
+  {path:'userDashboard',component:UserLoginDashboardComponent},
+  {path:'userHeader',component:UserLoginHeaderComponent},
+  {path:'restaurantLogin',component:RestaurantLoginComponent},
+  {path:'restaurantRegister',component:RestaurantSignupComponent},
+  {path:'restaurantDashboard',component:RestaurantDashboardComponent},
+  {path:'cart',component:CartComponent},
+  {path:'favourite',component:FavouriteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
