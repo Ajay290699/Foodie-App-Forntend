48509a6cc8e92c7ed9fb2d797c6a9f3add3f0773
Ajay Chavan <ajayc9838@gmail.com>
2023-04-25 17:30:05 +0530

    worked on restaurant-login working fine

--- src/app/components/restaurant-login/restaurant-login.component.ts
+++ src/app/components/restaurant-login/restaurant-login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { RestaurantAuthService } from 'src/app/service/restaurant-auth.service';
 
 @Component({
   selector: 'app-restaurant-login',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./restaurant-login.component.css']
 })
 export class RestaurantLoginComponent {
+
+  constructor(private builder:FormBuilder, private resAuthService:RestaurantAuthService, private router:Router){}
+
+  loginForm = new FormGroup({
+    'emailId':new FormControl('',[Validators.required, Validators.email]),
+    'password': new FormControl('',[Validators.required,Validators.minLength(8)])
+  })
+
+  get emailId(){
+    return this.loginForm.get('emailId');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
+
+  responseData:any;
+
+  sendLoginData(){
+    console.log(this.loginForm.value);
+    this.resAuthService.restaurantOwnerLogin(this.loginForm.value).subscribe({
+      next:response=>{
+        console.log(response);
+        this.responseData = response;
+        localStorage.setItem("restaurant-Owner-Token",this.responseData.token);
+        console.log(localStorage.getItem("restaurant-Owner-Token"));
+          this.router.navigateByUrl("restaurantDashboard");
+      }
+    })
+  }
 
 }
